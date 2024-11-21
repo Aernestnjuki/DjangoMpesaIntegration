33f1d08e750ab4766bfd6b006e5d694841b56c3e
aernest njuki <74731728+Aernestnjuki@users.noreply.github.com>
2024-11-21 20:47:20 +0300

    created a Transaction model

--- DjangoMpesaIntegration/models.py
+++ DjangoMpesaIntegration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+STATUS = (
+    (1, "Pending"),
+    (2, "Complete")
+)
+
+class Transaction(models.Model):
+    """This model records all the mpesa payment transactions"""
+    transaction_no = models.CharField(default=uuid.uuid4, max_length=50, unique=True)
+    phone_number = PhoneNumberField(null=False, blank=False)
+    checkout_request_id = models.CharField(max_length=200)
+    reference = models.CharField(max_length=40, blank=True)
+    description = models.TextField(null=True, blank=True)
+    amount = models.CharField(max_length=10)
+    status = models.CharField(max_length=15, choices=STATUS, default=1)
+    receipt_no = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    ip = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.transaction_no}'
